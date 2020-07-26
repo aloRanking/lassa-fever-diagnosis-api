2fd85426dd2d2c92587a5651485525c23d98f50a
aloRanking <adigunlateef02@gmail.com>
2020-07-26 11:09:35 +0100

    added findall method

--- src/main/java/com/aloranking/lfds/controllers/SurveyController.java
+++ src/main/java/com/aloranking/lfds/controllers/SurveyController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/survey")
 public class SurveyController {
@@ -19,6 +21,11 @@ public class SurveyController {
 
       return  surveyRepository.saveAndFlush(survey);
     }
+
+    @GetMapping
+    public List<Survey> getAllSurveys(){
+      return  surveyRepository.findAll();
+ }
 
 
 
