a9e0fa9eb9769245c1da59a0ed9ddc543a31ada2
aloRanking <adigunlateef02@gmail.com>
2020-07-26 11:08:18 +0100

    added Jwt request and response

---
+++ src/main/java/com/aloranking/lfds/models/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.aloranking.lfds.models;
+
+import java.io.Serializable;
+
+public class AuthenticationResponse implements Serializable {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
